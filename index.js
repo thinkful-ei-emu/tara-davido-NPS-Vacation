7541ec6fb905dca3ba1ef7d46523a5bcb1cb39e9
David Omstead <davidomstead@hotmail.com>
2019-06-11 14:25:57 -0700

    Functions

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 'use strict';
 
+const apiKey = "w4rEa9hoE0tiEke8xpTEJuSFgWllr1087DLeUvRB";
+
 //This function accepts the parameters(an object) and converts them into a string.  
 function formatQueryParams(params){
+  const queryItems = params.stateCode.split(" ");
+  const stateCodeArray = [];
+  queryItems.forEach(state =>{
+    stateCodeArray.push(`stateCode=${state}`);
+  });
+  return stateCodeArray.join('&');   
 
+
+//parks?stateCode=tx&stateCode=md&stateCode=&
 }
 
 //This line is going to take the input recieved from getParks and add the neccesary sections to the html file. 
@@ -15,7 +25,10 @@ function displayResults(responseJson,maxResults){
 //This function is going to accept input paramaters from a user and turn it into a searchable url. 
 //It then fetches the url based off the input params. it calls the function display results. 
 function getParks(query, MaxResults = 10){
-    const params = 
+  const params = {
+    apiKey,
+    stateCode: query,
+  }
 }
 
 // fetch(url,options)
